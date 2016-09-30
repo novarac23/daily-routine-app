b6f2ec47de3da75290e57b62b6b9cb2f59b1079e
Nikola Novakovic <nikola.novakovic9@gmail.com>
2016-09-29 21:19:53 -0400

    Allow users to do CRUD operations on routines

--- web/controllers/routine_controller.ex
+++ web/controllers/routine_controller.ex
@@ -14,22 +14,53 @@ defmodule DailyRoutine.RoutineController do
   end
 
   def new(conn, _params) do
-    changeset = Routine.changeset()
+    changeset = Routine.changeset(%Routine{})
 
-    render conn, "new.html"
+    render conn, "new.html", changeset: changeset
   end
 
-  # def create(conn, %{"routine" => routine_params}) do
-  #   changeset = Routine.changeset(routine_params)
+  def edit(conn, %{"id" => id}) do
+    routine = Repo.get!(Routine, id)
+    changeset = Routine.changeset(routine)
+    render conn, "edit.html", routine: routine, changeset: changeset
+  end
 
-  #   case Repo.insert!(changeset) do
-  #     {:ok, routine} ->
-  #       conn
-  #       |> put_flash(:info, "Your routine was successfully created!")
-  #       |> redirect(to: routine_path(conn, :index))
-  #     {:error, changeset} ->
-  #       render conn, "new.html", changeset: changeset
-  #   end
-  # end
+  def update(conn, %{"id" => id, "routine" => routine_params}) do
+    routine = Repo.get!(Routine, id)
+    changeset = Routine.changeset(routine, routine_params)
+
+    case Repo.update(changeset) do
+      {:ok, routine} ->
+        conn
+        |> put_flash(:info, "Your routine was successfully created!")
+        |> redirect(to: routine_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
+
+  end
+
+  def create(conn, %{"routine" => routine_params}) do
+    changeset = Routine.changeset(%Routine{}, routine_params)
+
+    case Repo.insert(changeset) do
+      {:ok, routine} ->
+        conn
+        |> put_flash(:info, "Your routine was successfully created!")
+        |> redirect(to: routine_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    routine = Repo.get(Routine, id)
+
+    Repo.delete!(routine)
+
+    conn
+		|> put_flash(:info, "Routine deleted successfully.")
+		|> redirect(to: routine_path(conn, :index))
+  end
 end
 
